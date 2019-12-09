af9d6f35325d2db5375f328db578c90b8151b87b
Marsel Radkevich <sqirelleonid@gmail.com>
2019-12-09 23:06:27 +0500

    Now you can throw dice or a coin. New classes: -DiceRollHandler - handles commands for actions with dice and a coin. -Thrower - implements a throw logic

--- src/main/java/tableBot/Constants.java
+++ src/main/java/tableBot/Constants.java
@@ -3,4 +3,9 @@ package tableBot;
 public class Constants
 {
     public static final String FILE_DOES_NOT_EXIST = "There's some problem with file. Please, try later";
+    public static final String LOW_ARGUMENTS_NUMBER = "Not enough arguments. Ig you have a problem ise '-%s help' to get help";
+    public static final String MANY_ARGUMENTS_NUMBER = "Too many arguments. If you have a problem use '-%s help' to get help.";
+    public static final String WRONG_ARGUMENTS_NUMBER = "wrong number of arguments. If you have a problem use '-%s help' to get help";
+    public static final String WRONG_ARGUMENTS_FORMAT = "Wrong arguments format. If you have a problem use '-%s help' to get help.";
+    public static final String UNKNOWN_COMMAND = "Unknown command. To know all the commands use '-help' to get help";
 }
